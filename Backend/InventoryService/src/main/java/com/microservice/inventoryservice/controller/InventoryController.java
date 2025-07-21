1a009a9dbfe6a860d3779692a0ad7b1b299c88fe
Admin <ravicv2000cv@gmail.com>
2025-07-21 18:51:09 +0530

    Added controller and service classes to check the item is in the stock.

---
+++ Backend/InventoryService/src/main/java/com/microservice/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,22 @@
+package com.microservice.inventoryservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.inventoryservice.service.InventoryService;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+	
+	@Autowired
+	private InventoryService inventoryService;
+
+	@GetMapping("/{skuCode}")
+	public boolean isInStock(@PathVariable String skuCode) {
+		return inventoryService.isInStock(skuCode);
+	}
+}
---
+++ Backend/InventoryService/src/main/java/com/microservice/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,13 @@
+package com.microservice.inventoryservice.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.microservice.inventoryservice.model.Inventory;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long>{
+
+	Optional<Inventory> findBySkuCode();
+
+}
